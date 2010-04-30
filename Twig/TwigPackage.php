664e32710a80f0e21adb11853a0847cebc684756
Juraj Blahunka <sul1nko@gmail.com>
2010-04-30 23:11:43 +0200

    Added Twig configuration

--- Twig/TwigPackage.php
+++ Twig/TwigPackage.php
@@ -16,7 +16,22 @@ class TwigPackage extends Package
 
 	function registerWiring(IContainerBuilder $container)
 	{
+		$container->setConstant('twig.options.auto_reload', true);
 
+		$container->define('twig_loader_service')
+			->setClass('Twig_Loader_Filesystem')
+			->addArgument('constant', 'view.paths');
+
+		$container->define('twig_environment_service')
+			->setClass('Twig_Environment')
+			->setArguments(array(
+				array('component', 'twig_loader_service'),
+				array('array', array(
+					'cache'       => array('constant', 'application.cache'),
+					'debug'       => array('constant', 'application.debug'),
+					'auto_reload' => array('constant', 'twig.options.auto_reload'),
+				)),
+			));
 	}
 
 	function registerPaths()
